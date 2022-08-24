6f459246e42bfc5ff2220eaa706b02cf69fc9e96
yaydy <107906477+yaydy@users.noreply.github.com>
2022-08-23 19:16:57 -0700

    cleaned up code and added hole

--- main.c
+++ main.c
@@ -1,28 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+int player[32];
 int players;
+int holes;
 int b;
 
+void getPlayers();
+void scoreTracker();
+void getHoles();
+void setPlayerValues();
+
 int main() {  
-  
-  puts("How many players are playing?");
-  scanf("%d", &players);
 
-  int player[32];
+getPlayers();
 
-for(int i = 0; i < players; i++){
-  player[i] = 0;
+getHoles();
+
+setPlayerValues();
+
+scoreTracker();
+  return 0;
 }
 
-for(;;){
-  for(int i = 0; i < players; i++){  
-    printf("Put the of score Player %d gained this round\n", i + 1);
-    scanf("%d", &b);
+void getPlayers(){
+  puts("How many players are playing?");
+  scanf("%d", &players);
+}
 
-    player[i] = player[i] + b;
+void getHoles(){
+  puts("How many holes are there?");
+  scanf("%d", &holes);
+}
+
+void setPlayerValues(){
+    for(int i = 0; i < players; i++){
+    player[i] = 0;
+  }
+}
+
+void scoreTracker(){
+  for(int i = 0; i < holes; i++){
+    for(int i = 0; i < players; i++){  
+      printf("Put the of score Player %d gained this round\n", i + 1);
+      scanf("%d", &b);
+
+      player[i] = player[i] + b;
   
-    printf("Player %d's score is now %d\n", i + 1, player[i]);
+      printf("Player %d's score is now %d\n", i + 1, player[i]);
     }
   }
-  return 0;
 }
